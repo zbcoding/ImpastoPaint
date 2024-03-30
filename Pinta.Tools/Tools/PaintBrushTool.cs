zbcoding/ImpastoPaint

commit 4d3410de1569bdcf8f4068fbcbda3d6383aaa4b9

For brushes, replacing `strokeColor` with `strokeContext` (#572)
* For brushes, replacing `strokeColor` with `strokeContext`

* Changed to `BrushStrokeArgs`, which contains more info

* Fixed formatting

* Removed `virtual` from certain methods

---------

Co-authored-by: Lehonti Ramos <lehonti@ramos>

Pinta.Tools/Tools/PaintBrushTool.cs
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 using System.Linq;
 using Cairo;
 using Gtk;
@@ -37,7 +38,6 @@ public sealed class PaintBrushTool : BaseBrushTool
 
 	private BasePaintBrush? default_brush;
 	private BasePaintBrush? active_brush;
-	private Color stroke_color;
 	private PointI last_point;
 
 	private const string BRUSH_SETTING = "paint-brush-brush";
@@ -89,25 +89,29 @@ public sealed class PaintBrushTool : BaseBrushTool
 
 	protected override void OnMouseMove (Document document, ToolMouseEventArgs e)
 	{
-		if (mouse_button == MouseButton.Left) {
-			stroke_color = Palette.PrimaryColor;
-		} else if (mouse_button == MouseButton.Right) {
-			stroke_color = Palette.SecondaryColor;
-		} else {
+		if (active_brush is null)
+			return;
+
+		if (mouse_button is not MouseButton.Left or MouseButton.Right) {
 			last_point = point_empty;
 			return;
 		}
 
-		if (active_brush is null)
-			return;
-
-		// TODO: also multiply by pressure
-		stroke_color = new Color (
-			stroke_color.R,
-			stroke_color.G,
-			stroke_color.B,
-			stroke_color.A * active_brush.StrokeAlphaMultiplier
-		);
+		// TODO: also multiply color by pressure
+		Color strokeColor = mouse_button switch {
+			MouseButton.Right => new (
+				Palette.SecondaryColor.R,
+				Palette.SecondaryColor.G,
+				Palette.SecondaryColor.B,
+				Palette.SecondaryColor.A * active_brush.StrokeAlphaMultiplier
+			),
+			MouseButton.Left or _ => new (
+				Palette.PrimaryColor.R,
+				Palette.PrimaryColor.G,
+				Palette.PrimaryColor.B,
+				Palette.PrimaryColor.A * active_brush.StrokeAlphaMultiplier
+			)
+		};
 
 		if (last_point.Equals (point_empty))
 			last_point = e.Point;
@@ -123,9 +127,11 @@ public sealed class PaintBrushTool : BaseBrushTool
 		g.LineWidth = brush_width;
 		g.LineJoin = LineJoin.Round;
 		g.LineCap = BrushWidth == 1 ? LineCap.Butt : LineCap.Round;
-		g.SetSourceColor (stroke_color);
+		g.SetSourceColor (strokeColor);
 
-		var invalidate_rect = active_brush.DoMouseMove (g, stroke_color, surf, e.Point, last_point);
+		BrushStrokeArgs strokeArgs = new (strokeColor, e.Point, last_point);
+
+		var invalidate_rect = active_brush.DoMouseMove (g, surf, strokeArgs);
 
 		// If we draw partially offscreen, Cairo gives us a bogus
 		// dirty rectangle, so redraw everything.

Pinta.Core/Classes/BasePaintBrush.cs
@@ -60,24 +60,22 @@ public abstract class BasePaintBrush
 	/// </summary>
 	public virtual double StrokeAlphaMultiplier => 1;
 
-	public virtual void DoMouseUp ()
+	public void DoMouseUp ()
 	{
 		OnMouseUp ();
 	}
 
-	public virtual void DoMouseDown ()
+	public void DoMouseDown ()
 	{
 		OnMouseDown ();
 	}
 
-	public virtual RectangleI DoMouseMove (
+	public RectangleI DoMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
-		return OnMouseMove (g, strokeColor, surface, current, last);
+		return OnMouseMove (g, surface, strokeArgs);
 	}
 
 	/// <summary>
@@ -108,8 +106,6 @@ public abstract class BasePaintBrush
 	/// <returns>A rectangle containing the area of the canvas that should be redrawn.</returns>
 	protected abstract RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last);
+		BrushStrokeArgs strokeArgs);
 }

Pinta.Core/Classes/BrushStrokeArgs.cs
@@ -1,0 +1,21 @@
+using Cairo;
+
+namespace Pinta.Core;
+
+public sealed class BrushStrokeArgs
+{
+	public Color StrokeColor { get; }
+	public PointI CurrentPosition { get; }
+	public PointI LastPosition { get; }
+
+	public BrushStrokeArgs (
+		Color strokeColor,
+		PointI currentPosition,
+		PointI lastPosition
+	)
+	{
+		StrokeColor = strokeColor;
+		CurrentPosition = currentPosition;
+		LastPosition = lastPosition;
+	}
+}

Pinta.Tools/Brushes/CircleBrush.cs
@@ -39,15 +39,13 @@ public sealed class CircleBrush : BasePaintBrush
 
 	protected override RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
-		PointI mouseDelta = current - last;
+		PointI mouseDelta = strokeArgs.CurrentPosition - strokeArgs.LastPosition;
 		PointD center = new (
-			X: Math.Floor (current.X / 100.0) * 100 + 50,
-			Y: Math.Floor (current.Y / 100.0) * 100 + 50
+			X: Math.Floor (strokeArgs.CurrentPosition.X / 100.0) * 100 + 50,
+			Y: Math.Floor (strokeArgs.CurrentPosition.Y / 100.0) * 100 + 50
 		);
 		double d = mouseDelta.Magnitude () * 2.0;
 		int steps = Random.Next (1, 10);

Pinta.Tools/Brushes/GridBrush.cs
@@ -39,26 +39,24 @@ public sealed class GridBrush : BasePaintBrush
 
 	protected override RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
 		PointD c = new (
-			X: Math.Round (current.X / 100.0) * 100.0,
-			Y: Math.Round (current.Y / 100.0) * 100.0
+			X: Math.Round (strokeArgs.CurrentPosition.X / 100.0) * 100.0,
+			Y: Math.Round (strokeArgs.CurrentPosition.Y / 100.0) * 100.0
 		);
 
 		PointD d = new (
-			X: (c.X - current.X) * 10.0,
-			Y: (c.Y - current.Y) * 10.0
+			X: (c.X - strokeArgs.CurrentPosition.X) * 10.0,
+			Y: (c.Y - strokeArgs.CurrentPosition.Y) * 10.0
 		);
 
 		for (int i = 0; i < 50; i++) {
 			g.MoveTo (c.X, c.Y);
 			g.QuadraticCurveTo (
-				current.X + Random.NextDouble () * d.X,
-				current.Y + Random.NextDouble () * d.Y,
+				strokeArgs.CurrentPosition.X + Random.NextDouble () * d.X,
+				strokeArgs.CurrentPosition.Y + Random.NextDouble () * d.Y,
 				c.X,
 				c.Y);
 			g.Stroke ();

Pinta.Tools/Brushes/PlainBrush.cs
@@ -38,17 +38,15 @@ public sealed class PlainBrush : BasePaintBrush
 
 	protected override RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
 		// Cairo does not support a single-pixel-long single-pixel-wide line
-		bool isSinglePixelLine = IsSinglePixelLine (g, current, last);
+		bool isSinglePixelLine = IsSinglePixelLine (g, strokeArgs);
 		if (isSinglePixelLine)
-			DrawSinglePixelLine (g, current);
+			DrawSinglePixelLine (g, strokeArgs);
 		else
-			DrawNonSinglePixelLine (g, current, last);
+			DrawNonSinglePixelLine (g, strokeArgs);
 
 		RectangleI dirty = g.StrokeExtents ().ToInt ();
 
@@ -59,26 +57,26 @@ public sealed class PlainBrush : BasePaintBrush
 		return inflated;
 	}
 
-	private static bool IsSinglePixelLine (Context g, PointI current, PointI last)
+	private static bool IsSinglePixelLine (Context g, BrushStrokeArgs strokeArgs)
 	{
 		return
-			(current.X == last.X) &&
-			(current.Y == last.Y) &&
+			(strokeArgs.CurrentPosition.X == strokeArgs.LastPosition.X) &&
+			(strokeArgs.CurrentPosition.Y == strokeArgs.LastPosition.Y) &&
 			(g.LineWidth == 1) &&
-			PintaCore.Workspace.ActiveWorkspace.PointInCanvas ((PointD) current)
+			PintaCore.Workspace.ActiveWorkspace.PointInCanvas ((PointD) strokeArgs.CurrentPosition)
 		;
 	}
 
-	private static void DrawSinglePixelLine (Context g, PointI current)
+	private static void DrawSinglePixelLine (Context g, BrushStrokeArgs strokeArgs)
 	{
-		g.Rectangle (current.X, current.Y, 1.0, 1.0);
+		g.Rectangle (strokeArgs.CurrentPosition.X, strokeArgs.CurrentPosition.Y, 1.0, 1.0);
 		g.Fill ();
 	}
 
-	private static void DrawNonSinglePixelLine (Context g, PointI current, PointI last)
+	private static void DrawNonSinglePixelLine (Context g, BrushStrokeArgs strokeArgs)
 	{
-		g.MoveTo (last.X + 0.5, last.Y + 0.5);
-		g.LineTo (current.X + 0.5, current.Y + 0.5);
+		g.MoveTo (strokeArgs.LastPosition.X + 0.5, strokeArgs.LastPosition.Y + 0.5);
+		g.LineTo (strokeArgs.CurrentPosition.X + 0.5, strokeArgs.CurrentPosition.Y + 0.5);
 		g.StrokePreserve ();
 	}
 }

Pinta.Tools/Brushes/SplatterBrush.cs
@@ -39,15 +39,15 @@ public sealed class SplatterBrush : BasePaintBrush
 
 	protected override RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
 		int line_width = (int) g.LineWidth;
 
 		// we want a minimum size of 2 for the splatter (except for when the brush width is 1), since a splatter of size 1 is very small
 		int size = (line_width == 1) ? 1 : Random.Next (2, line_width);
+
+		PointI current = strokeArgs.CurrentPosition;
 
 		RectangleD rect = new (
 			x: current.X - Random.Next (-15, 15),

Pinta.Tools/Brushes/SquaresBrush.cs
@@ -36,11 +36,12 @@ public sealed class SquaresBrush : BasePaintBrush
 
 	protected override RectangleI OnMouseMove (
 		Context g,
-		Color strokeColor,
 		ImageSurface surface,
-		PointI current,
-		PointI last)
+		BrushStrokeArgs strokeArgs)
 	{
+		PointI current = strokeArgs.CurrentPosition;
+		PointI last = strokeArgs.LastPosition;
+
 		PointI mouseDelta = current - last;
 		PointD rotated = new ( // 90 degrees
 			X: -mouseDelta.Y,

commit 3b49b92f6da26697ce8ce392525425f7ce7b31e9

Fix incorrect pattern matching introduced in 4d3410de
This caused the RMB to not work in the paintbrush tool

Fixes: #755

Pinta.Tools/Tools/PaintBrushTool.cs
@@ -92,7 +92,7 @@ public sealed class PaintBrushTool : BaseBrushTool
 		if (active_brush is null)
 			return;
 
-		if (mouse_button is not MouseButton.Left or MouseButton.Right) {
+		if (mouse_button is not (MouseButton.Left or MouseButton.Right)) {
 			last_point = point_empty;
 			return;
 		}
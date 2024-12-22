zbcoding/ImpastoPaint

commit 3884898db737b7f6f381959548327cf842ade04e

Fix issues with grabbing focus in the canvas widget
Doing this on the drawing area widget (which is inside a scroll window) seems to cause the scrollbars to reset back to 0. Grabbing focus on the parent canvas widget works better.

Fixes: #698

Pinta.Gui.Widgets/Widgets/Canvas/CanvasWindow.cs
@@ -59,9 +59,11 @@ public sealed class CanvasWindow : Grid
 		scroll_controller.OnScroll += HandleScrollEvent;
 		vp.AddController (scroll_controller);
 
+		// The mouse handler in PintaCanvas grabs focus away from toolbar widgets.
+		Focusable = true;
+
 		Canvas = new PintaCanvas (this, document) {
 			Name = "canvas",
-			CanFocus = true,
 		};
 
 		// Rulers

Pinta.Gui.Widgets/Widgets/Canvas/PintaCanvas.cs
@@ -45,8 +45,6 @@ public sealed class PintaCanvas : DrawingArea
 		CanvasWindow = window;
 		this.document = document;
 
-		Focusable = true;
-
 		cr = new CanvasRenderer (true, true);
 
 		// Keep the widget the same size as the canvas
@@ -77,8 +75,10 @@ public sealed class PintaCanvas : DrawingArea
 			// Note we don't call click_controller.SetState (Gtk.EventSequenceState.Claimed) here, so
 			// that the CanvasWindow can also receive motion events to update the root window mouse position.
 
-			// A mouse click on the canvas should grab focus from any toolbar widgets, etc
-			GrabFocus ();
+			// A mouse click on the canvas should grab focus away from any toolbar widgets, etc
+			// Using the root canvas widget works best - if the drawing area is given focus, the scroll
+			// widget jumps back to the origin.
+			CanvasWindow.GrabFocus ();
 
 			// The canvas gets the button press before the tab system, so
 			// if this click is on a canvas that isn't currently the ActiveDocument yet, 

commit abbccd2276124b6dc2d324539f1e78040ecb1293

Fix regression with canvas focus introduced by 3884898d
When the root canvas window was made focusable instead of the canvas drawing area widget, this broke other code which explicitly was trying to make the drawing area focusable. All code now tries to set focus on the root canvas window, which from the changes in 3884898d is the ideal choice to avoid unexpected scrolling while zoomed in

Fixes: #2070032

Pinta.Core/Classes/DocumentWorkspace.cs
@@ -52,6 +52,7 @@ public sealed class DocumentWorkspace
 
 	#region Public Properties
 	public Gtk.DrawingArea Canvas { get; set; } = null!; // NRT - This is set soon after creation
+	public Gtk.Widget CanvasWindow { get; set; } = null!; // NRT - This is set soon after creation
 
 	/// <summary>
 	/// Returns whether the zoomed image fits in the window without requiring scrolling.
@@ -175,6 +176,19 @@ public sealed class DocumentWorkspace
 	public void InvalidateWindowRect (RectangleI windowRect)
 	{
 		OnCanvasInvalidated (new CanvasInvalidatedEventArgs (windowRect));
+	}
+
+	/// <summary>
+	/// Grabs focus to the canvas widget. This can be used to avoid leaving focus in
+	/// toolbar widgets, for example.
+	/// </summary>
+	public void GrabFocusToCanvas ()
+	{
+		bool gained_focus = CanvasWindow.GrabFocus ();
+		// Log a warning if something went wrong, e.g. there is a non-focusable widget
+		// in the hierarchy.
+		if (!gained_focus)
+			Console.Error.WriteLine ("Failed to gain focus on the canvas widget!");
 	}
 
 	/// <summary>

Pinta.Core/Extensions/Gtk/GtkExtensions.Widget.cs
@@ -120,7 +120,7 @@ partial class GtkExtensions
 		// tools can handle subsequent key events.
 		spin.OnValueChanged += (o, e) => {
 			if (PintaCore.Workspace.HasOpenDocuments)
-				PintaCore.Workspace.ActiveWorkspace.Canvas.GrabFocus ();
+				PintaCore.Workspace.ActiveWorkspace.GrabFocusToCanvas ();
 		};
 		return spin;
 	}

Pinta.Gui.Widgets/Widgets/Canvas/CanvasWindow.cs
@@ -79,9 +79,9 @@ public sealed class CanvasWindow : Gtk.Grid
 		scroll_controller.OnDecelerate += (_, _) => gesture_zoom.IsActive (); // Cancel scroll deceleration when zooming
 		vp.AddController (scroll_controller);
 
-		// The mouse handler in PintaCanvas grabs focus away from toolbar widgets.
+		// The mouse handler in PintaCanvas grabs focus away from toolbar widgets, along
+		// with DocumentWorkpace.GrabFocusToCanvas()
 		Focusable = true;
-
 		Canvas = new PintaCanvas (this, document, canvasGrid) { Name = "canvas" };
 
 		// Rulers

Pinta.Tools/Tools/TextTool.cs
@@ -334,7 +334,7 @@ public sealed class TextTool : BaseTool
 	private void HandleFontChanged (object? sender, EventArgs e)
 	{
 		if (workspace.HasOpenDocuments)
-			workspace.ActiveDocument.Workspace.Canvas.GrabFocus ();
+			workspace.ActiveDocument.Workspace.GrabFocusToCanvas ();
 
 		UpdateFont ();
 	}

Pinta/MainWindow.cs
@@ -117,7 +117,7 @@ internal sealed class MainWindow
 
 	private void Workspace_DocumentClosed (object? sender, DocumentEventArgs e)
 	{
-		var tab = FindTabWithCanvas ((PintaCanvas) e.Document.Workspace.Canvas);
+		var tab = FindTabWithCanvas ((CanvasWindow) e.Document.Workspace.CanvasWindow);
 
 		if (tab != null)
 			canvas_pad.Notebook.RemoveTab (tab);
@@ -164,6 +164,7 @@ internal sealed class MainWindow
 			RulersVisible = PintaCore.Actions.View.Rulers.Value,
 			RulerMetric = GetCurrentRulerMetric ()
 		};
+		doc.Workspace.CanvasWindow = canvas;
 		doc.Workspace.Canvas = canvas.Canvas;
 
 		DocumentViewContent my_content = new (doc, canvas);
@@ -223,9 +224,9 @@ internal sealed class MainWindow
 		// first shot at handling the event if
 		// the mouse pointer is on the canvas
 		if (PintaCore.Workspace.HasOpenDocuments) {
-			var canvas_window = ((PintaCanvas) PintaCore.Workspace.ActiveWorkspace.Canvas).CanvasWindow;
+			var canvas_window = (CanvasWindow) PintaCore.Workspace.ActiveWorkspace.CanvasWindow;
 
-			if ((canvas_window.Canvas.HasFocus || canvas_window.IsMouseOnCanvas) &&
+			if ((canvas_window.HasFocus || canvas_window.IsMouseOnCanvas) &&
 				 canvas_window.Canvas.DoKeyPressEvent (controller, args)) {
 				return true;
 			}
@@ -249,9 +250,9 @@ internal sealed class MainWindow
 		// Give the Canvas (and by extension the tools)
 		// first shot at handling the event if
 		// the mouse pointer is on the canvas
-		var canvas_window = ((PintaCanvas) PintaCore.Workspace.ActiveWorkspace.Canvas).CanvasWindow;
+		var canvas_window = (CanvasWindow) PintaCore.Workspace.ActiveWorkspace.CanvasWindow;
 
-		if (canvas_window.Canvas.HasFocus || canvas_window.IsMouseOnCanvas)
+		if (canvas_window.HasFocus || canvas_window.IsMouseOnCanvas)
 			canvas_window.Canvas.DoKeyReleaseEvent (controller, args);
 	}
 
@@ -575,10 +576,10 @@ internal sealed class MainWindow
 			int image_x = PintaCore.Workspace.ImageSize.Width;
 			int image_y = PintaCore.Workspace.ImageSize.Height;
 
-			var canvas_window = PintaCore.Workspace.ActiveWorkspace.Canvas.Parent!;
+			var canvas_viewport = PintaCore.Workspace.ActiveWorkspace.Canvas.Parent!;
 
-			int window_x = canvas_window.GetAllocatedWidth ();
-			int window_y = canvas_window.GetAllocatedHeight ();
+			int window_x = canvas_viewport.GetAllocatedWidth ();
+			int window_y = canvas_viewport.GetAllocatedHeight ();
 
 			double ratio =
 				(image_x / (double) window_x >= image_y / (double) window_y)
@@ -608,16 +609,16 @@ internal sealed class MainWindow
 		PintaCore.Actions.View.ResumeZoomUpdate ();
 
 		var doc = PintaCore.Workspace.ActiveDocument;
-		var tab = FindTabWithCanvas ((PintaCanvas) doc.Workspace.Canvas);
+		var tab = FindTabWithCanvas ((CanvasWindow) doc.Workspace.CanvasWindow);
 
 		if (tab != null)
 			canvas_pad.Notebook.ActiveItem = tab;
 
-		doc.Workspace.Canvas.GrabFocus ();
+		doc.Workspace.GrabFocusToCanvas ();
 	}
 
-	private IDockNotebookItem? FindTabWithCanvas (PintaCanvas canvas) =>
+	private IDockNotebookItem? FindTabWithCanvas (CanvasWindow canvas_window) =>
 		canvas_pad.Notebook.Items
-		.Where (i => ((CanvasWindow) i.Widget).Canvas == canvas)
+		.Where (i => ((CanvasWindow) i.Widget) == canvas_window)
 		.FirstOrDefault ();
 }